6cf3df86ef827d7ef1847103a7af8a3d8a2dedc9
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-04-14 21:46:35 +0300

    Add CRUD

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,10 +5,61 @@ module.exports.taskCreated = async (req, res, next) => {
     const {
       body: { body },
     } = req;
-    const taskCreated = await Task.create({body});
-    res.status(200).send({data:taskCreated});
+    const taskCreated = await Task.create({ body });
+    res.status(201).send({ data: taskCreated });
+  } catch (err) {
+    next(err);
+  }
+};
+module.exports.getAllTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.findAll();
+    res.status(201).send({ data: tasks });
   } catch (err) {
     next(err);
   }
 };
 
+module.exports.updateTask = async (req, res, next) => {
+  try {
+    const {
+      body: { body },
+      params: { idTask },
+    } = req;
+    const updatedTask = await Task.update({body}, {
+      where: { id: idTask },
+      returning: true,
+    });
+    res.status(201).send({ data: updatedTask });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getTask = async (req, res, next) => {
+  try {
+    const {
+      params: { idTask },
+    } = req;
+    const task = await Task.findByPk(Number(idTask));
+    res.status(201).send(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteTask = async (req, res, next) => {
+  try {
+    const {
+      params: { idTask },
+    } = req;
+    const taskDelete = await Task.destroy({
+      where: {
+        id: idTask,
+      },
+    });
+    res.status(201).send({ data: taskDelete });
+  } catch (err) {
+    next(err);
+  }
+};
